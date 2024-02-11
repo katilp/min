dd51ab32a945e47f2e15136b198a56eb84a09381
ElisaPerini <elisa.k.perini@gmail.com>
2024-02-11 09:24:51 +0100

    added linter

--- frontend/min-app/src/App.tsx
+++ frontend/min-app/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Mineral from "./pages/Mineral";
 import Header from "./components/Header";
@@ -9,15 +9,15 @@ import Header from "./components/Header";
 //TODO: change /minerals to /mineral/:id
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/minerals" element={<Mineral/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home/>} />
+				<Route path="/minerals" element={<Mineral/>} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
